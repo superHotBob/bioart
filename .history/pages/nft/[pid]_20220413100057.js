5c7b366071037187332e76977facf95345a167c9
AlexHor1909 <19ham09@mail.com>
2022-04-13 11:05:40 +0300

    without error

--- .history/pages/nft/[pid]_20220413100057.js
+++ .history/pages/nft/[pid]_20220413100057.js
@@ -946,7 +946,7 @@ export default function NFT() {
             padding-top: 10px;
           }
           .content p {
-            margin-top: 40px;
+            margin-top: 80px;
           }
           .image {
             height: 110vw;
